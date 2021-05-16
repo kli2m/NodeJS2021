868b858b39bcb3e79de430d8fcf832372ecff700
kli2m <ivanklimm87@gmail.com>
2021-05-16 16:39:50 +0300

    feat: 3 tests passed

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,13 @@
 const usersRepo = require('./user.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async () =>  usersRepo.getAll();
 
-module.exports = { getAll };
+const get = async id =>  usersRepo.get(id);
+
+const create = async user =>  usersRepo.create(user)
+
+const put = async (id, user) =>  usersRepo.put(id, user);
+
+const del = async id =>   usersRepo.del(id);
+
+module.exports = { getAll, get, create, put, del };
